620394335242c3dd347a3866f916f08935faacef
SAVINDUGUNATHILAKA <savindugunathilaka08@gmail.com>
2026-08-05 20:48:15 +0530

    fix: comment out unused localStorage operations and improve code clarity

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-console.log("Hii");
+// console.log("Hii");
 
-// // localStorage.clear()
+// localStorage.clear()
 
-localStorage.setItem("name", "ranil");
-localStorage.setItem("name", "wimal");
-localStorage.setItem("name1", "kamal");
+// localStorage.setItem("name", "ranil");
+// localStorage.setItem("name", "wimal");
+// localStorage.setItem("name1", "kamal");
 
 
-localStorage.removeItem("name1");
+// localStorage.removeItem("name1");
 
-let custName = localStorage.getItem("name");
+// let custName = localStorage.getItem("name");
 
-console.log(custName);
+// console.log(custName);
+
+// localStorage.clear();
+
+//------------------------------------------------------------------------------------
+
+let customer = {
+    name: "kamal",
+    age: 12,
+    isActive: true,
+}
+
+let stringCustomer = JSON.stringify(customer);
+
+localStorage.setItem("customer", stringCustomer);
+
+let retCustomer = localStorage.getItem("customer");
+
+let jsonCustoemr = JSON.parse(retCustomer);
+
+console.log(jsonCustoemr.name);
+
+console.log(customer);

36c659bff090a79656274d22ade374aca4e072d9
SAVINDUGUNATHILAKA <savindugunathilaka08@gmail.com>
2026-08-13 00:56:52 +0530

    fix: integrate weather API to display current weather information in content section

--- app.js
+++ app.js
@@ -139,13 +139,36 @@
 //--------------------------------------------------------------------------------
 
 
+// const apiKey = "fa66d55bfb24497380e85722243011"
+
+// const baseUrl = "http://api.weatherapi.com/v1"
+
+// fetch(`${baseUrl}/current.json?key=${apiKey}&q=panadura`).then(res => res.json()).then(data => {
+//      console.log(data);
+
+// }
+
+
+
 
 
 const apiKey = "4f33e736620b40099a984953261208"
 
 const baseUrl = "http://api.weatherapi.com/v1"
 
-fetch(`${baseUrl}/current.json?key=${apiKey}&q=panadura`).then(res=>res.json()).then(data=>{
-    console.log();
-    
+fetch(`${baseUrl}/current.json?key=${apiKey}&q=panadura`).then(res => res.json()).then(data => {
+    console.log(data);
+
+
+    document.getElementById("contentSection").innerHTML = `
+ <div>
+                <h1>${data.current.condition.text}</h1>
+                <h1>${data.location.name}</h1>
+                <img src="${data.current.condition.icon}" alt>
+                <p>${data.location.country}</p>
+                <p>${data.current.temp_c}</p>
+            </div>
+    `
+
+
 })

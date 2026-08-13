fac548d554faab44667d6e2c0e42310720d00b27
SAVINDUGUNATHILAKA <savindugunathilaka08@gmail.com>
2026-08-13 20:53:21 +0530

    feat: add geolocation functionality to log user's current position

--- app.js
+++ app.js
@@ -193,3 +193,13 @@ function btnSearchOnAction() {
     })
 
 }
+
+
+navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+
+
+});
+
